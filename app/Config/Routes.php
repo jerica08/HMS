e387dfcaf6f3c8ba0393d5bf22c88f775a0ce1ac
spiegel123 <enzopro2222@gmail.com>
2025-09-27 15:49:18 +0800

    contact routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,3 +7,6 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 $routes->get('about', 'Home::about');
+$routes->get('contact', 'Home::contact');
+
+$routes->setAutoRoute(true);

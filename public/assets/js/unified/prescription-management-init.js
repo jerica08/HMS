a656eca362e76d474633f560f7471395a7110a69
jerica08 <jericamarquez88@gmail.com>
2025-12-12 20:33:56 +0800

    commit

--- public/assets/js/unified/prescription-management-init.js
+++
@@ -1,8 +0,0 @@
-// Helper to expose closePrescriptionBillingModal globally without re-instantiating PrescriptionManager
-(function() {
-    window.closePrescriptionBillingModal = function() {
-        if (window.prescriptionManager && typeof window.prescriptionManager.closeBillingModal === 'function') {
-            window.prescriptionManager.closeBillingModal();
-        }
-    };
-})();
